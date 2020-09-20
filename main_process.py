cea66e86cba3d1d42b7691c5ef75e302b4635779
pikabol88 <nena.2000@mail.ru>
2020-09-20 08:29:39 +0300

    Start of the integration module

--- main_process.py
+++ main_process.py
@@ -4,17 +4,19 @@ import vacancies_filter
 from person_work_experience import person_work_experience
 from read_file_to_list import read_file_to_list
 
-with open("resume_sorted.csv",encoding="utf-8") as table:
+with open("resume_sorted.csv", encoding="utf-8") as table:
     reader = csv.DictReader(table, delimiter=";")
     person = Person()
     synonyms = read_file_to_list("synonyms.txt")
     category = "кладовщик"
+    cout = 0
     for row in reader:
         if not person.is_equal(list(row.values())[0]):
             personDict = vacancies_filter.prepare_date_for_filter(person)
             personDict = vacancies_filter.vacancies_filter(personDict, synonyms)
             if personDict:
+                cout += 1
                 personDict = person_work_experience(personDict, category)
-                print(personDict)
-
-        person.feel(list(row.values()))
+                #print(personDict)
+        person.fill([x.lower() for x in list(row.values())])
+    print(cout)
