464ea91209bf8d425cd2fec398d7fd0ca7c22822
maxherobrine <47478509+maxherobrine@users.noreply.github.com>
2020-09-20 14:16:08 +0300

    Update person_work_experience.py

--- person_work_experience.py
+++ person_work_experience.py
@@ -1,38 +1,5 @@
 import datetime as DT
 
-'''
-person1 = {'id': 12345, 'category': ['лол', 'инженер', 'технолог'],
-           'startDate': ['2020-08-08', '2019-07-07', '2018-04-05'],
-           'endDate': ['2021-08-08', '2020-01-07', '2018-06-05']}
-person2 = {'id': 12345, 'category': ['кулинар', 'инженер', 'технолог'],
-           'startDate': ['2020-08-08', '2019-07-07', '2018-04-05'],
-           'endDate': ['2021-08-08', '2020-01-07', '2018-06-05']}
-
-'''
-'''
-def person_work_experience(personInformation:dict, category:str):
-    person = {'id':personInformation.get('id'), category: False, 'profession': [], 'experience': []}
-    count = 0
-    isChecked = 0
-    for name in personInformation.get('category'):
-        if name == category:
-            person.update({category: True})
-            experience = calculate_work_time(personInformation.get('startDate')[count], personInformation.get('endDate')[count])
-            person.update({'experience': experience})
-            isChecked = 1
-        count+=1
-    if not isChecked:
-        count = 0
-        works = []
-        experience = []
-        for name in personInformation.get('category'):
-            works.append(name)
-            experience.append(calculate_work_time(personInformation.get('startDate')[count], personInformation.get('endDate')[count])) #Функция вычисление времени работы
-            count+=1
-        person.update({'profession': works, 'experience': experience})
-    return  person
-'''
-
 
 def person_work_experience(personInformation: dict, category: str):
     person = {'id': personInformation.get('id'), category: False, 'time': 0, 'profession': [], 'experience': [], 'search': []}
@@ -87,12 +54,3 @@ def other_experience(profession:list, experience:list):
         otherExp.update({profession[i]: experience[i]})
 
     return otherExp
-
-
-
-
-'''
-print(person_work_experience(person1, 'лол'))
-print(person_work_experience(person2, 'лол'))
-'''
-
